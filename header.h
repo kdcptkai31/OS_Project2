3e90b929c4dcfc7b51a7e307ce72b08ce714a52b
Kai Duty <kdcptkai31@gmail.com>
2020-05-04 21:46:15 -0700

    Lint and Refactor

--- header.h
+++ header.h
@@ -1,11 +1,10 @@
 #pragma once
+
 #include "process.h"
 #include "queue.h"
 #include "memory.h"
 #include <string>
 
-// get user input
-void get_input();
 // store a list of processes based on the provided file name
 void readData();
 
@@ -15,12 +14,6 @@ void add_new_proc_to_waitList(int current_time);
 // returns a string (T = x) or a tab (\t) based on whether anything's been
 // announced this clock tick
 string correct_format_for_time(int current_time);
-
-// the main program loop
-void clock_loop();
-
-// the main function
-int main();
 
 // assigns any available memory to waiting procs that'll fit in there
 void assign_memory_for_process(int current_time);
--- process.h
+++ process.h
@@ -1,13 +1,15 @@
 #pragma once
+
 class PROCESS{
 public:
-        int pid;
-        int arriving_time;
-        int burst_time;
-        int request_memory_size;
 
-        int time_added_to_memory;
-        int is_active;
-        int time_finished;
+  int pid;
+  int arriving_time;
+  int burst_time;
+  int request_memory_size;
+  int time_added_to_memory;
+  int is_active;
+  int time_finished;
+
 };
 
