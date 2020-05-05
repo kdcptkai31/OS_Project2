b479d06752291cd2f03af23eb2244235715c5c79
Kai Duty <kdcptkai31@gmail.com>
2020-05-04 17:22:34 -0700

    Initial Program

---
+++ Makefile
@@ -0,0 +1,12 @@
+all: main
+
+main: main.o
+	g++ main.o -o main
+
+main.o: main.cpp
+	g++ -c main.cpp
+
+tar:
+	tar -cvf assignment2_Duty-Thompson-Webber.tar *.cpp *.h *.md *.png
+
+clean: rm *.o assignment2_Duty-Thompson-Webber.tar
---
+++ process.h
@@ -0,0 +1,13 @@
+#pragma once
+class PROCESS{
+public:
+        int pid;
+        int arriving_time;
+        int burst_time;
+        int request_memory_size;
+
+        int time_added_to_memory;
+        int is_active;
+        int time_finished;
+};
+
